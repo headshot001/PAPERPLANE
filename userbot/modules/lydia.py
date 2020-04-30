10e4be4ecb81e7ea1c307714d14a51882b7a8d04
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-04-30 13:56:39 +0530

    Create lydia.py

---
+++ userbot/modules/lydia.py
@@ -0,0 +1,61 @@
+from coffeehouse.lydia import LydiaAI
+from coffeehouse.api import API
+import asyncio
+from userbot import LYDIA_API_KEY
+from userbot import BOTLOG, CMD_HELP, bot
+from userbot.events import register
+
+ACC_LYDIA = {}
+if LYDIA_API_KEY:
+    api_key = LYDIA_API_KEY
+    api_client = API(api_key)
+    lydia = LydiaAI(api_client)
+
+@register(outgoing=True, disable_errors=True, pattern="^.autochat$")
+async def addcf(event):
+    if event.fwd_from:
+        return
+    reply_message = await event.get_reply_message()
+    await event.edit("Running...")
+    await asyncio.sleep(4)
+    await event.edit("Still Running....")
+    reply_msg = await event.get_reply_message()
+    if reply_msg:
+        session = lydia.create_session()
+        session_id = session.id
+        if reply_msg.from_id is None:
+            return await event.edit("Invalid user type.")
+        ACC_LYDIA.update({(event.chat_id & reply_msg.from_id): session})
+        await event.edit("Auto reply activated for user: {} in chat: {}".format(str(reply_msg.from_id), str(event.chat_id)))
+    else:
+        await event.edit("Tag any user's message to activate on them")
+
+@register(outgoing=True, disable_errors=True, pattern="^.stopchat$")
+async def remcf(event):
+    if event.fwd_from:
+        return
+    await event.edit("Running..")
+    await asyncio.sleep(4)
+    await event.edit("Processing...")
+    reply_msg = await event.get_reply_message()
+    try:
+        del ACC_LYDIA[event.chat_id & reply_msg.from_id]
+        await event.edit(" Auto reply disabled for user: {} in chat: {}".format(str(reply_msg.from_id), str(event.chat_id)))
+    except Exception:
+        await event.edit("This person does not have activated auto reply on him/her.")
+
+@register(incoming=True, disable_errors=True, disable_edited=True)
+async def user(event):
+    user_text = event.text
+    try:
+        session = ACC_LYDIA[event.chat_id & event.from_id]
+        msg = event.text
+        async with event.client.action(event.chat_id, "typing"):
+            text_rep = session.think_thought(msg)
+            wait_time = 0
+            for i in range(len(text_rep)):
+                wait_time = wait_time + 0.1
+            await asyncio.sleep(wait_time)
+            await event.reply(text_rep)
+    except (KeyError, TypeError):
+        return
