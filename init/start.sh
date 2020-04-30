b6de4461e7aa689d729dadaeff5d7330ba7e1981
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-01 04:45:04 +0530

    Update start.sh

--- init/start.sh
+++ init/start.sh
@@ -1,10 +1,9 @@
-#! /usr/bin/env bash
+#!/bin/bash
 # Copyright (C) 2019 The Raphielscape Company LLC.
 #
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
 
-apt-get update && add-apt-repository universe && apt-get install -y redis-server
 redis-server --daemonize yes
 python3 -m userbot
