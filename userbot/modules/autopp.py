9bf10ac49dcbd13d46477bdbd19d6b0d32c0464d
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-05 20:22:32 +0530

    Update autopp.py

--- userbot/modules/autopp.py
+++ userbot/modules/autopp.py
@@ -13,25 +13,25 @@ import urllib.request
 from telethon import events
 from userbot import CMD_HELP
 url = 'https://raw.githubusercontent.com/Ayush1311/PAPERPLANE/master/Antaro.ttf'
-url1 = 'https://telegra.ph/file/bd2d8bae73546db5719db.jpg'
 urllib.request.urlretrieve(url, './Antaro.ttf')
-urllib.request.urlretrieve(url1, './original_pic.png')
 FONT_FILE_TO_USE = "./Antaro.ttf"
-downloaded_file_name = "./original_pic.png"
-photo = "./new_photo.png"
-
+url1 = 'https://telegra.ph/file/d3048ee6593a0bcb327a3.png'
+urllib.request.urlretrieve(url1, './original_pic.png')
 
 @register(outgoing=True, pattern="^.autopp$")
 async def autopic(event):
     while True:
+        downloaded_file_name = "./original_pic.png"
+        photo = "./new_photo.png"
         shutil.copy(downloaded_file_name, photo)
         LT = datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
         OT = LT.strftime("root@ayush:~# python3\n\n>>>import datetime\n\n>>>localtime = datetime.datetime.now()\n\n>>>while True :\n\n...     print(' ',localtime)\n\n...     print(' ','HAVE A NICE DAY !')\n\n...else :\n\n...     break\n\n>>> %d.%m.%y %H:%M\n\n>>> HAVE A NICE DAY !")
         img = Image.open(photo)
         drawn_text = ImageDraw.Draw(img)
         fnt = ImageFont.truetype(FONT_FILE_TO_USE, 25)
-        drawn_text.text((40, 80), OT, font=fnt, fill="red")
+        drawn_text.text((40, 80), OT, font=fnt, fill="black")
         img.save(photo)
+        file = await event.client.upload_file(photo) 
         try:
             await event.client(functions.photos.UploadProfilePhotoRequest(file))
             os.remove(photo)
