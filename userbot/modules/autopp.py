9576a695550e0cc0a50d171854550a0c51a997a8
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-05 19:59:33 +0530

    Update autopp.py

--- userbot/modules/autopp.py
+++ userbot/modules/autopp.py
@@ -4,10 +4,8 @@ import os
 import datetime
 import time
 from PIL import Image, ImageDraw, ImageFont
-from pySmartDL import SmartDL
 from telethon.tl import functions
 from userbot.events import register
-AUTO_PP = os.environ.get("AUTO_PP", "https://telegra.ph/file/bd2d8bae73546db5719db.jpg")
 import asyncio
 import shutil
 import pytz 
@@ -15,22 +13,18 @@ import urllib.request
 from telethon import events
 from userbot import CMD_HELP
 url = 'https://raw.githubusercontent.com/Ayush1311/PAPERPLANE/master/Antaro.ttf'
+url1 = 'https://telegra.ph/file/bd2d8bae73546db5719db.jpg'
 urllib.request.urlretrieve(url, './Antaro.ttf')
+urllib.request.urlretrieve(url1, './original_pic.png')
 FONT_FILE_TO_USE = "./Antaro.ttf"
+downloaded_file_name = "./original_pic.png"
+photo = "./new_photo.png"
+
 
 @register(outgoing=True, pattern="^.autopp$")
 async def autopic(event):
-    downloaded_file_name = "./original_pic.png"
-    downloader = SmartDL(AUTO_PP, downloaded_file_name, progress_bar=True)
-    downloader.start(blocking=False)
-    photo = "photo_pfp.png"
-    while not downloader.isFinished():
-        place_holder = None
-    counter = -30
     while True:
         shutil.copy(downloaded_file_name, photo)
-        im = Image.open(photo)
-        file_test = im.rotate(counter, expand=False).save(photo, "PNG")
         LT = datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
         OT = LT.strftime("root@ayush:~# python3\n\n>>>import datetime\n\n>>>localtime = datetime.datetime.now()\n\n>>>while True :\n\n...     print(' ',localtime)\n\n...     print(' ','HAVE A NICE DAY !')\n\n...else :\n\n...     break\n\n>>> %d.%m.%y %H:%M\n\n>>> HAVE A NICE DAY !")
         img = Image.open(photo)
@@ -38,17 +32,12 @@ async def autopic(event):
         fnt = ImageFont.truetype(FONT_FILE_TO_USE, 25)
         drawn_text.text((40, 80), OT, font=fnt, fill="red")
         img.save(photo)
-        file = await event.client.upload_file(photo)  # pylint:disable=E0602
         try:
-            await event.client(functions.photos.UploadProfilePhotoRequest(  # pylint:disable=E0602
-                file
-            ))
+            await event.client(functions.photos.UploadProfilePhotoRequest(file))
             os.remove(photo)
-            counter -= 30
             await asyncio.sleep(60)
         except:
             return
-
 
 CMD_HELP.update({
 "autoname":
