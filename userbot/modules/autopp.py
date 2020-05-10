f13e52c32cd01cfdfff9d857fa6eaecc9860def9
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-10 23:08:12 +0530

    Update autopp.py

--- userbot/modules/autopp.py
+++ userbot/modules/autopp.py
@@ -35,15 +35,26 @@ async def autopic(event):
         img.save(photo)
         file = await event.client.upload_file(photo) 
         try:
-            lim = 1
-            pfplist = await bot(GetUserPhotosRequest(user_id=event.from_id, offset=0, max_id=0, limit=lim))
-            input_photos = []
-            for sep in pfplist.photos:
-                input_photos.append(InputPhoto(id=sep.id, access_hash=sep.access_hash, file_reference=sep.file_reference))
-            await event.client(DeletePhotosRequest(id=input_photos))
-            await event.client(UploadProfilePhotoRequest(file))
-            os.remove(photo)
-            await asyncio.sleep(60)
+            group = event.text[8:]
+            if group == 'all':
+                  lim = 0
+            elif group.isdigit():
+                  lim = int(group)
+        else:
+          lim = 1
+        pfplist = await bot(GetUserPhotosRequest(user_id=delpfp.from_id,
+                             offset=0,
+                             max_id=0,
+                             limit=lim))
+        input_photos = []
+        for sep in pfplist.photos:
+            input_photos.append(InputPhoto(id=sep.id,
+                             access_hash=sep.access_hash,
+                             file_reference=sep.file_reference))
+        await bot(DeletePhotosRequest(id=input_photos))
+        await event.client(UploadProfilePhotoRequest(file))
+        os.remove(photo)
+        await asyncio.sleep(60)
             
         except:
             return
