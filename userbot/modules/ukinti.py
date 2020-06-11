15c6a0fdb809c0deadca70cc08dccebfa97f9c24
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-11 11:06:15 +0530

    Update ukinti.py

--- userbot/modules/ukinti.py
+++ userbot/modules/ukinti.py
@@ -10,8 +10,6 @@ from telethon.tl.functions.channels import (EditAdminRequest,
                                             EditPhotoRequest)
 from userbot.events import register
 
-KICK_RIGHTS = ChatBannedRights(until_date=None, view_messages=True)
-c = 0
 
 @register(pattern=".kickthefools")
 async def _(event):
@@ -23,7 +21,8 @@ async def _(event):
     if not admin and not creator:
         await event.reply("I am not admin here !")
         return
-
+    c = 0
+    KICK_RIGHTS = ChatBannedRights(until_date=None, view_messages=True)
     await event.reply("Searching Participant Lists...")
     async for i in event.client.iter_participants(event.chat_id):
 
