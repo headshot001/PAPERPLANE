f72a16dbd684cf8098542f151fdd9cd24356230f
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-16 14:45:31 +0530

    Update scrapers.py

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -44,7 +44,7 @@ async def img_sampler(event):
     # creating list of arguments
     downloader.download(query, limit=lim)
     os.chdir(f'dataset/bing/{query}')
-    for listed in glob.glob("*.jpg") and for listed in glob.glob("*.png") and for listed in glob.glob("*.jpeg"):     
+    for listed in glob.glob("*.jpg") or listed in glob.glob("*.png") or listed in glob.glob("*.jpeg"):     
         await event.client.send_file(event.chat_id, listed)
     os.system('rm -rf dataset')
     os.chdir('/root/haruka')
