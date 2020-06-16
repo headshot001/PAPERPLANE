45dca846aad39a2545da6a1ae88e2ef05e6d8735
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-16 16:54:26 +0530

    Update ukinti.py

--- userbot/modules/ukinti.py
+++ userbot/modules/ukinti.py
@@ -25,6 +25,7 @@ async def _(event):
         return
     c = 0
     KICK_RIGHTS = ChatBannedRights(until_date=None, view_messages=True)
+    UserStatusLongAgo = UserStatusLastMonth + UserStatusLastWeek
     await event.reply("Searching Participant Lists...")
     async for i in event.client.iter_participants(event.chat_id):
 
@@ -42,12 +43,12 @@ async def _(event):
             else:
                c = c + 1       
 
-        if isinstance(i.status, UserStatusLastMonth+UserStatusLastWeek):
+        if isinstance(i.status, UserStatusLongAgo):
             status = await event.client(EditBannedRequest(event.chat_id, i, KICK_RIGHTS))
             if not status:
                return
             else:
                c = c + 1
-
+    
     required_string = "Successfully Kicked **{}** users"
     await event.reply(required_string.format(c))
