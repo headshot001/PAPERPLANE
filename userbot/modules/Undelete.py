d10bd352531b9f73483e2859be7a2b4f77607df2
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-16 19:21:17 +0530

    Create Undelete.py

---
+++ userbot/modules/Undelete.py
@@ -0,0 +1,16 @@
+from telethon import events
+from usebot.events import register
+import asyncio
+
+
+@register(pattern="undelete")
+async def _(event):
+    if event.fwd_from:
+        return
+    c = await event.get_chat()
+    if c.admin_rights or c.creator:
+        a = await event.client.get_admin_log(event.chat_id,limit=1, search="", edit=False, delete=True)
+        for i in a:
+          await event.reply(i.original.action.message)
+    else:
+        await event.edit("You need administrative permissions in order to do this command")
