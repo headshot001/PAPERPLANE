4bb5b91129763aebabf53c42be1ac21ffda8da8a
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-21 13:43:54 +0530

    Update ukinti.py

--- userbot/modules/ukinti.py
+++ userbot/modules/ukinti.py
@@ -69,8 +69,12 @@ async def get_user_from_id(user, event):
 
     return user_obj
 
+import cv2
+import numpy as np
+import mapper
+import os
 
-@register(pattern="^/ascii")
+@register(pattern="^/camscanner")
 async def asciiart(event):
   if event.fwd_from:
      return  
@@ -78,9 +82,41 @@ async def asciiart(event):
      await event.reply("Reply To A Image Plox..")
      return
   reply_msg = await event.get_reply_message()
-  if not os.path.isdir('./'):
-        os.makedirs('./')
   downloaded_file_name = await event.client.download_media(reply_msg, './')
-  os.system(f'python3 ascii {downloaded_file_name}')
-  omg = await event.client.upload_file('./ascii.html')
+
+  image=cv2.imread(downloaded_file_name)   #read in the image
+  image=cv2.resize(image,(1300,800)) #resizing because opencv does not work well with bigger images
+  orig=image.copy()
+  gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)  #RGB To Gray Scale
+  cv2.imshow("Title",gray)
+
+  blurred=cv2.GaussianBlur(gray,(5,5),0)  #(5,5) is the kernel size and 0 is sigma that determines the amount of blur
+  cv2.imshow("Blur",blurred)
+
+  edged=cv2.Canny(blurred,30,50)  #30 MinThreshold and 50 is the MaxThreshold
+  cv2.imshow("Canny",edged)
+
+  contours,hierarchy=cv2.findContours(edged,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)  #retrieve the contours as a list, with simple apprximation model
+  contours=sorted(contours,key=cv2.contourArea,reverse=True)
+
+  for c in contours:
+    p=cv2.arcLength(c,True)
+    approx=cv2.approxPolyDP(c,0.02*p,True)
+
+    if len(approx)==4:
+        target=approx
+        break
+  approx=mapper.mapp(target) #find endpoints of the sheet
+
+  pts=np.float32([[0,0],[800,0],[800,800],[0,800]])  #map to 800*800 target window
+
+  op=cv2.getPerspectiveTransform(approx,pts)  #get the top or bird eye view effect
+  dst=cv2.warpPerspective(orig,op,(800,800))
+
+  cv2.imwrite("./scanned.pdf", dst)
+        
+  omg = await event.client.upload_file('./scanned.pdf')
+
+
   await event.client.send_file(event.chat_id, omg)
+  os.remove("./scanned.pdf")
